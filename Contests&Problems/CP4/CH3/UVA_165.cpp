d63188e07be9b02a70888e372115bf36cde92258
= <=>
2022-07-20 19:00:06 -0300

    testing repo on windows

---
+++ Contests&Problems/CP4/CH3/UVA_165.cpp
@@ -0,0 +1,45 @@
+/*
+author: eiji_tnpb
+*/
+#include <bits/stdc++.h>
+using namespace std;
+#define endl '\n'
+#define pb push_back
+#define REP(idx,x) for(int i = idx; i<x; i++)
+#define REPN(idx,x) for(int i = idx; i<=x; i++)
+#define all(x) x.begin(),x.end()
+#define pq priority_queue
+typedef long long ll;
+typedef vector<int> vi;
+typedef vector<ll> vll;
+typedef vector<vi> vvi;
+typedef vector<vll> vvll;
+typedef vector<bool> vbool;
+typedef pair<int,int> pii;
+typedef vector<pii> vpii;
+typedef vector<vpii> vvpii;
+const int INF = 0x3f3f3f3f;
+const ll INFLL = 8e18;
+const int mx = 1e6;
+int R, C;
+// N L O S NE SE SO NO
+int dr[] = {-1, 0, 1, 0, 1, -1, -1, 1};
+int dc[] = {0, 1, 0, -1, 1, 1, -1, -1};
+//g++ -std=c++17 main.cpp -o main && time ./main
+//g++ -std=c++17 -Wall main.cpp -o main && time ./main
+
+void solve()
+{
+
+}
+
+int main()
+{
+    cin.tie(0);
+    ios::sync_with_stdio(0);
+    // freopen("input.txt", "r", stdin);
+    // freopen("output.txt", "w", stdout);
+    solve();
+    return 0;
+}
+
